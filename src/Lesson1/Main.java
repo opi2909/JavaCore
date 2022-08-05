561038ee29bcb9ba8dc62d5689ee976a51955df8
opi2909 <o.p.ivanova@outlook.com>
2022-08-05 23:33:32 +0300

    Lesson1.2

--- src/Lesson1/Main.java
+++ src/Lesson1/Main.java
@@ -3,14 +3,9 @@ package Lesson1;
 public class Main {
     public static void main(String[] args) {
         new Team("Pobeda");
-        TeamMembers[] member = new TeamMembers[4];
-        member[0] = new TeamMembers("Masha", "female", 80);
-        member[1] = new TeamMembers("Ira", "female", 84);
-        member[2] = new TeamMembers("Igor", "male", 100);
-        member[3] = new TeamMembers("Stat", "male", 55);
-        for (int i = 0; i <= 3; i++) {
-            member[i].infoAboutTeamMembers();
+        Team.infoTeamMembers();
         }
 
-    }
+
+
 }
